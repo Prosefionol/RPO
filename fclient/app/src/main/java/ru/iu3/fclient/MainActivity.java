9e2543a0db992f6b8805cac7657e24046adf61f0
Prosefionol <ilya_kuper@mail.ru>
2021-03-08 02:11:17 +0300

    Laba 2 content

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -10,12 +10,19 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("mbedcrypto");
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        byte[] rnd = randomBytes(16);
+        byte[] data = {1,2,3,4,5,6,7,8};
+        byte[] encText = encrypt(rnd, data);
+        byte[] decText = decrypt(rnd, encText);
+
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
@@ -27,4 +34,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
