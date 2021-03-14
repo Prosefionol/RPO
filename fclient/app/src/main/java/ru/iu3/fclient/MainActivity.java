79d9fef8305f3f4da6423913d6f9a5e8a5d5d3b4
Prosefionol <ilya_kuper@mail.ru>
2021-03-14 20:54:11 +0300

    Laba2 micro changes

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
+        initRng();
     }
 
     @Override
